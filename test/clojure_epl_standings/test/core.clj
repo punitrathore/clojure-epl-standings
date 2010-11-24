61432e1876cc59044e551a62c08b258e2e65eddc
Punit Rathore <punit@MinasTirith.local>
2010-11-24 22:26:59 +0530

    adding initial project files

---
+++ test/clojure_epl_standings/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-epl-standings.test.core
+  (:use [clojure-epl-standings.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
