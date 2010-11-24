3e479faa48e4204857e80d1f91fa57b1a92e8631
Punit Rathore <punit@MinasTirith.local>
2010-11-24 22:32:42 +0530

    added enlive to dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-epl-standings "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Page scraping from espnstar.com to fetch EPL standings"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0-SNAPSHOT"]])
