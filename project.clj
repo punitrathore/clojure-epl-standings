b58060651a721995432f8b24d47ddcef27b08a60
Punit Rathore <punit@MinasTirith.local>
2010-11-25 08:21:21 +0530

    fixing namespace

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [enlive "1.0.0-SNAPSHOT"]]
-  :main clojure-epl-standings
+  :main clojure-epl-standings.core
   )
